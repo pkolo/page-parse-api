7869b525addec5354fb6dfb8355cfda7fcb47382
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-19 11:50:24 -0400

    Tests for valid tag value

--- spec/models/header_spec.rb
+++ spec/models/header_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe Header, type: :model do
   # Test validations
   it { should validate_presence_of(:content) }
   it { should validate_presence_of(:tag) }
+  it { should validate_inclusion_of(:tag).in_array(["h1", "h2", "h3"])}
 end
