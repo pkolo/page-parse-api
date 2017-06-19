bba6122813b71f4f64094e99f695c4e1068a4dfc
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-19 15:57:35 -0400

    Creates new link and assoc headers; Returns JSON of new link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,11 +6,18 @@ class LinksController < ApplicationController
     render :json => @links, include: { headers: {only: [:tag, :content]} }
   end
 
-  # POST /links
   def create
     @link = Link.new(url: params[:url])
     if @link.save
-      render :json => @link, status: 201
+      parsed_body = Nokogiri::HTML(open(params['url']))
+
+      parsed_body.css('h1', 'h2', 'h3').each do |element|
+        new_header = Header.new(content: element.text, tag: element.name)
+        new_header.link = @link
+        new_header.save
+      end
+
+      render :json => @link, include: { headers: {only: [:tag, :content]} }, status: 201
     else
       render :json => {:errors => @link.errors.full_messages}, status: 422
     end
