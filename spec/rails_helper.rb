6698be91ba2fc7f94c2b67883aab00e519402e11
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-19 09:41:54 -0400

    Adds faker to spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'shoulda-matchers'
 require 'factory_girl_rails'
+require 'faker'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
