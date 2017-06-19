a1fcbd8a4b9c7f8b7cd46c5e93623cec1fbf18dd
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-19 15:40:36 -0400

    Creates new link and returns status

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,4 +6,14 @@ class LinksController < ApplicationController
     render :json => @links, include: { headers: {only: [:tag, :content]} }
   end
 
+  # POST /links
+  def create
+    @link = Link.new(url: params[:url])
+    if @link.save
+      render :json => @link, status: 201
+    else
+      render :json => {:errors => @link.errors.full_messages}, status: 422
+    end
+  end
+
 end
