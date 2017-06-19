26f2754dc30c1c9b69676d29941dbd5aacc18e49
Patrick Kolodgy <pkolodgy@gmail.com>
2017-06-19 17:14:28 -0400

    Adds URL validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,20 @@
+class UrlValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attribute, value)
+    valid = begin
+      URI.parse(value).kind_of?(URI::HTTP)
+    rescue URI::InvalidURIError
+      false
+    end
+    unless valid
+      record.errors[attribute] << (options[:message] || "is an invalid URL")
+    end
+  end
+
+end
+
 class Link < ApplicationRecord
   has_many :headers, dependent: :destroy
 
-  validates :url, presence: true
+  validates :url, presence: true, url: true
 end
